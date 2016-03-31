6168bf19bc7229fedc5ab7ce894c263bbee82f2b
mac <mac@192.168.0.156>
2016-04-01 00:51:11 +0200

    Trabaja terminado

--- src/dam_ed04_actividad/CCuenta.java
+++ src/dam_ed04_actividad/CCuenta.java
@@ -5,20 +5,48 @@
 
 package dam_ed04_actividad;
 
+/**
+ * Clase CCuenta con las variables para guardar el nombre, número de cuenta, 
+ * saldo y tipo de interés del cliente. Tiene los métodos getters y setters y
+ * un método para ingresar dinero y otro para retirar.
+ * @author Mikel Gil
+ * @version 1.0
+ */
 
 public class CCuenta {
 
-
+    /**
+     * Variable de clase para almacenar el nombre
+     */
     private String nombre;
+    /**
+     * Variable de clase para almacenar el número de cuenta
+     */
     private String cuenta;
+    /**
+     * Variable de clase para almacenar el saldo
+     */
     private double saldo;
+    /**
+     * Variable de clase para almacenar el tipo de interés
+     */
     private double tipoInterés;
 
-
+    /**
+     * Constructor sin parámetros
+     */
     public CCuenta()
     {
     }
-
+    
+    /**
+     * Constructor con parametros para inicializar las variables con los valores
+     * deseado
+     * @param nom
+     * @param cue
+     * @param sal
+     * @param tipo 
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -27,23 +55,12 @@ public class CCuenta {
         tipoInterés = tipo;
     }
 
-    public void asignarNombre(String nom)
-    {
-        setNombre(nom);
-    }
-
-    public String obtenerNombre()
-    {
-        return getNombre();
-    }
-
-
-    public double estado()
-    {
-        return getSaldo();
-    }
-
-
+   /**
+    * Método para ingresar dinero en la cuenta. Modifica el saldo y lanza una 
+    * excepción si se intenta ingresar un valor negativo
+    * @param cantidad
+    * @throws Exception 
+    */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -51,64 +68,81 @@ public class CCuenta {
         setSaldo(getSaldo() + cantidad);
     }
 
+    /**
+     * Método para retirar dinero de la cuenta. Modifica el saldo y lanza dos 
+     * excepciones. Una cuando se intenta retirar una cantidad negativa y otra 
+     * cuando no hay suficiente saldo
+     * @param cantidad
+     * @throws Exception 
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
             throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado()< cantidad)
+        if (getSaldo()< cantidad)
             throw new Exception ("No se hay suficiente saldo");
         setSaldo(getSaldo() - cantidad);
     }
 
-
-    public String obtenerCuenta()
-    {
-        return getCuenta();
-    }
-
-
+    /**
+     * Método para obtener el nombre
+     * @return nombre 
+     */
     public String getNombre() {
         return nombre;
     }
 
-
+    /**
+     * Método para poner el nombre
+     * @param nombre 
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-
+    /**
+     * Método para obtener el tipo de interés
+     * @return 
+     */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
-
+    /**
+     * Método para poner el tipo de interés
+     * @param tipoInterés 
+     */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
     }
 
     /**
-     * @return the cuenta
+     * Método para obtener el número de cuenta
+     * @return cuenta
      */
     public String getCuenta() {
         return cuenta;
     }
 
     /**
-     * @param cuenta the cuenta to set
+     * Método para poner el número de cuenta
+     * @param cuenta
      */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
     /**
-     * @return the saldo
+     * Método para obtener el saldo
+     * @return saldo
      */
     public double getSaldo() {
         return saldo;
     }
 
     /**
-     * @param saldo the saldo to set
+     * Método para poner el saldo
+     * @param saldo
      */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
--- src/dam_ed04_actividad/Main.java
+++ src/dam_ed04_actividad/Main.java
@@ -5,19 +5,27 @@
 
 package dam_ed04_actividad;
 
-
+/**
+ * Clase Main con el método principal para probar la clase CCuenta
+ * @author Mikel Gil
+ * @version 1.0
+ */
 public class Main {
 
+    /**
+     * Método principal main
+     * @param args 
+     */
     public static void main(String[] args) {
         operativa_cuenta(0);
     }
-
+  
     private static void operativa_cuenta(float cantidad) {
         CCuenta cuenta1;
         double saldoActual;
 
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = cuenta1.estado();
+        saldoActual = cuenta1.getSaldo();
         System.out.println("El saldo actual es"+ saldoActual );
 
         try {
